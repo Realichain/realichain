a205373e7c7305ecdafe4ce2b61d7d9abf88c44c
Realichain <miciosauro68@yahoo.com>
2024-08-25 08:36:34 +0200

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -29,7 +29,7 @@ extern const std::string CURRENCY_UNIT;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 16795000 * COIN;
+static const CAmount MAX_MONEY = 8300000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /**
